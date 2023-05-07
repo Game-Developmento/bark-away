5f834132cf3bdbdfe2570aa2db80dd3c2e1d8dcf
oriHoward <ori.howard@gmail.com>
2023-05-07 20:52:26 +0300

    Added eventHandler, and updated TaskManager to choose random tasks to appear

--- Assets/Scripts/TaskManagerUI.cs
+++ Assets/Scripts/TaskManagerUI.cs
@@ -15,14 +15,32 @@ public class TaskManagerUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        TaskManager.Instance.OnTaskSpawned += TaskManager_OnTaskSpawned;
+        TaskManager.Instance.OnTaskCompleted += Instance_OnTaskCompleted;
+        UpdateVisual();
 
     }
+
+    private void TaskManager_OnTaskSpawned(object sender, System.EventArgs E)
+    {
+        UpdateVisual();
+    }
+    private void Instance_OnTaskCompleted(object sender, System.EventArgs E)
+    {
+        UpdateVisual();
+    }
+
     private void UpdateVisual()
     {
         foreach (Transform child in container)
         {
             if (child == taskTemplate) continue;
             Destroy(child.gameObject);
+        }
+        foreach (TasksObjectSO task in TaskManager.Instance.GetTasksObjectSOList())
+        {
+            Transform taskTransform = Instantiate(taskTemplate, container);
+            taskTransform.gameObject.SetActive(true);
         }
     }
     // Update is called once per frame
