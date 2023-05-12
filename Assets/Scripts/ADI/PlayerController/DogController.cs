ca25ddddfdd131b537ec3b1018874eed0dc413de
adiy55 <adiyafe55@gmail.com>
2023-05-12 12:34:05 +0300

    Update rotations for dog movement

--- Assets/Scripts/ADI/PlayerController/DogController.cs
+++ Assets/Scripts/ADI/PlayerController/DogController.cs
@@ -56,7 +56,7 @@ public class DogController : MonoBehaviour
         if (isMovementPressed)
         {
             float targetAngle = Mathf.Atan2(currentMovement.x, currentMovement.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime * Time.deltaTime);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
