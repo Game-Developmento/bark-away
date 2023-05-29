a68297e02da381386c00b3d121cb76c7f33a0454
OriHoward <ori.howard@gmail.com>
2023-05-29 18:14:50 +0300

    Updated MiniMap script

--- Assets/Scripts/MiniMap.cs
+++ Assets/Scripts/MiniMap.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class MiniMap : MonoBehaviour
 {
-    public Transform player;
-    private void LateUpdate() {
-        Vector3 newPosition = player.position;
-        newPosition.y = transform.position.y;
-        transform.position = newPosition;
-        transform.rotation = Quaternion.Euler(90f,Camera.main.transform.eulerAngles.y,0f);
+    private void LateUpdate()
+    {
+        transform.rotation = Quaternion.Euler(90f, Camera.main.transform.eulerAngles.y, 0f);
     }
 }
