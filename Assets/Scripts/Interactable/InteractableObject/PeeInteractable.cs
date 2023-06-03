97023d1944fa046c3ffcf8fb3a5df2c746bbebe9
adiy55 <adiyafe55@gmail.com>
2023-06-03 14:47:43 +0300

    Fix dog animations + destroy pee prefab after 15 seconds

--- Assets/Scripts/Interactable/InteractableObject/PeeInteractable.cs
+++ Assets/Scripts/Interactable/InteractableObject/PeeInteractable.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using System.Collections;
 
 public class PeeInteractable : InteractableBase
 {
     [SerializeField] private GameObject prefabToSpawn;
     [SerializeField] private GameObject nearestPlayer; // Set nearest player during interactions
     private Animator playerAnimator;
+    [SerializeField] private float destoryPeeDelay = 15f;
     public override void StartInteraction(GameObject player)
     {
         // Initilalize current player variables
@@ -41,7 +43,24 @@ public class PeeInteractable : InteractableBase
         Debug.Log("Cleanup PeeInteractable!");
         Vector3 nearestPlayerPosition = nearestPlayer.transform.position;
         Vector3 positionToSpawn = new Vector3(nearestPlayerPosition.x, transform.position.y, nearestPlayerPosition.z);
-        Instantiate(prefabToSpawn, positionToSpawn, prefabToSpawn.transform.rotation);
+        GameObject spawnedPee = Instantiate(prefabToSpawn, positionToSpawn, prefabToSpawn.transform.rotation);
+        StartCoroutine(DestroyPee(spawnedPee.gameObject));
+        // TODO: Check why this works only in children
+        SpriteRenderer renderer = this.gameObject.GetComponentInChildren<SpriteRenderer>();
+        if (renderer != null)
+        {
+            Debug.Log("TryGetComponent");
+            renderer.enabled = false;
+        }
+
+    }
+
+    private IEnumerator DestroyPee(GameObject spawnedPee)
+    {
+        Debug.Log("Start DestroyPee");
+        yield return new WaitForSeconds(destoryPeeDelay);
+        Destroy(spawnedPee);
+        Debug.Log("DestroyPee");
         Destroy(gameObject);
     }
 }
