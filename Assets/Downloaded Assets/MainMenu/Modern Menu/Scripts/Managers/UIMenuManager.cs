e76395a6f3e750c0705fe04d33b91d03e9dc8871
OriHoward <ori.howard@gmail.com>
2023-06-05 14:12:08 +0300

    Added first version of main menu

---
+++ Assets/Downloaded Assets/MainMenu/Modern Menu/Scripts/Managers/UIMenuManager.cs
@@ -0,0 +1,204 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+namespace SlimUI.ModernMenu
+{
+    public class UIMenuManager : MonoBehaviour
+    {
+        private Animator CameraObject;
+
+        // campaign button sub menu
+        [Header("MENUS")]
+        [Tooltip("The Menu for when the MAIN menu buttons")]
+        public GameObject mainMenu;
+        [Tooltip("THe first list of buttons")]
+        public GameObject firstMenu;
+        [Tooltip("The Menu for when the PLAY button is clicked")]
+        public GameObject playMenu;
+        [Tooltip("The Menu for when the EXIT button is clicked")]
+        public GameObject exitMenu;
+
+        public enum Theme { custom1, custom2, custom3 };
+        [Header("THEME SETTINGS")]
+        public Theme theme;
+        private int themeIndex;
+        public ThemedUIData themeController;
+
+        [Header("PANELS")]
+        [Tooltip("The UI Panel parenting all sub menus")]
+        public GameObject mainCanvas;
+    
+
+        [Header("LOADING SCREEN")]
+        public bool waitForInput = true;
+        public GameObject loadingMenu;
+        [Tooltip("The loading bar Slider UI element in the Loading Screen")]
+        public Slider loadingBar;
+        public TMP_Text loadPromptText;
+        public KeyCode userPromptKey;
+
+        [Header("SFX")]
+        [Tooltip("The GameObject holding the Audio Source component for the HOVER SOUND")]
+        public AudioSource hoverSound;
+        [Tooltip("The GameObject holding the Audio Source component for the AUDIO SLIDER")]
+        public AudioSource sliderSound;
+        [Tooltip("The GameObject holding the Audio Source component for the SWOOSH SOUND when switching to the Settings Screen")]
+        public AudioSource swooshSound;
+
+        void Start()
+        {
+            CameraObject = transform.GetComponent<Animator>();
+            playMenu.SetActive(false);
+            exitMenu.SetActive(false);
+            firstMenu.SetActive(true);
+            mainMenu.SetActive(true);
+
+            SetThemeColors();
+        }
+
+        void SetThemeColors()
+        {
+            switch (theme)
+            {
+                case Theme.custom1:
+                    themeController.currentColor = themeController.custom1.graphic1;
+                    themeController.textColor = themeController.custom1.text1;
+                    themeIndex = 0;
+                    break;
+                case Theme.custom2:
+                    themeController.currentColor = themeController.custom2.graphic2;
+                    themeController.textColor = themeController.custom2.text2;
+                    themeIndex = 1;
+                    break;
+                case Theme.custom3:
+                    themeController.currentColor = themeController.custom3.graphic3;
+                    themeController.textColor = themeController.custom3.text3;
+                    themeIndex = 2;
+                    break;
+                default:
+                    Debug.Log("Invalid theme selected.");
+                    break;
+            }
+        }
+
+        public void PlayGame()
+        {
+            exitMenu.SetActive(false);
+            playMenu.SetActive(true);
+            
+        }
+
+        public void LoadLevel1() {
+            // load scene of level 1;
+            LoadScene("MainMenu");
+        }
+        public void LoadLevel2() {
+            // load scene of level 2;
+            LoadScene("MainMenu");
+        }
+        public void LoadLevel3() {
+            // load scene of level 3;
+            LoadScene("MainMenu");
+        }
+
+
+        // public void ReturnMenu()
+        // {
+        //     playMenu.SetActive(false);
+        //     exitMenu.SetActive(false);
+        //     mainMenu.SetActive(true);
+        // }
+
+        public void LoadScene(string scene)
+        {
+            if (scene != "")
+            {
+                StartCoroutine(LoadAsynchronously(scene));
+            }
+        }
+
+        public void DisablePlayGame()
+        {
+            playMenu.SetActive(false);
+        }
+
+        public void LoadTutorialScene()
+        {
+            DisablePlayGame();
+            CameraObject.SetFloat("Animate", 1);
+            // need to change here to tutorial scene
+            LoadScene("MainMenu");
+        }
+
+        public void Position1()
+        {
+            CameraObject.SetFloat("Animate", 0);
+        }
+
+        public void PlayHover()
+        {
+            hoverSound.Play();
+        }
+
+        public void PlaySFXHover()
+        {
+            sliderSound.Play();
+        }
+
+        public void PlaySwoosh()
+        {
+            swooshSound.Play();
+        }
+
+        // Are You Sure - Quit Panel Pop Up
+        public void AreYouSure()
+        {
+            exitMenu.SetActive(true);
+            DisablePlayGame();
+        }
+
+        public void QuitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+				Application.Quit();
+#endif
+        }
+
+        // Load Bar synching animation
+        IEnumerator LoadAsynchronously(string sceneName)
+        { // scene name is just the name of the current scene being loaded
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+            operation.allowSceneActivation = false;
+            mainCanvas.SetActive(false);
+            loadingMenu.SetActive(true);
+
+            while (!operation.isDone)
+            {
+                float progress = Mathf.Clamp01(operation.progress / .95f);
+                loadingBar.value = progress;
+
+                if (operation.progress >= 0.9f && waitForInput)
+                {
+                    loadPromptText.text = "Press " + userPromptKey.ToString().ToUpper() + " to continue";
+                    loadingBar.value = 1;
+
+                    if (Input.GetKeyDown(userPromptKey))
+                    {
+                        operation.allowSceneActivation = true;
+                    }
+                }
+                else if (operation.progress >= 0.9f && !waitForInput)
+                {
+                    operation.allowSceneActivation = true;
+                }
+
+                yield return null;
+            }
+        }
+    }
+}
