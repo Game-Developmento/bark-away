422ddc4b1d9c40baae2f8a6dddbe32793b95f413
oriHoward <ori.howard@gmail.com>
2023-06-09 03:00:04 +0300

    Modified clock script to work backwards

--- Assets/Scripts/UI/ClockUI.cs
+++ Assets/Scripts/UI/ClockUI.cs
@@ -5,29 +5,28 @@ public class ClockUI : MonoBehaviour
 {
     [SerializeField] private Transform clockHourHandTransform;
     [SerializeField] private TextMeshProUGUI timeText;
-
+    [SerializeField] private float startTimeInSeconds = 60f;
     private float clockRotationSpeed = 6f; // Degrees per second for clock rotation
-    private float startTime;
-    private float secondsPerHour = 3600f;
     private float secondsPerMinute = 60f;
-
-    private void Start()
-    {
-        startTime = Time.time;
-    }
 
     private void Update()
     {
-        clockHourHandTransform.Rotate(Vector3.forward, -clockRotationSpeed * Time.deltaTime);
-
         // Time calculation
-        float timePassed = Time.time - startTime;
-        int minutes = Mathf.FloorToInt((timePassed % secondsPerHour) / secondsPerMinute);
-        int seconds = Mathf.FloorToInt(timePassed % secondsPerMinute);
+        float remainingSeconds = startTimeInSeconds - Time.time;
+        if (remainingSeconds > 0)
+        {
+            clockHourHandTransform.Rotate(Vector3.forward, clockRotationSpeed * Time.deltaTime);
+            int minutes = Mathf.FloorToInt(remainingSeconds / secondsPerMinute);
+            int seconds = Mathf.FloorToInt(remainingSeconds % secondsPerMinute);
 
-        string minutesString = minutes.ToString("00");
-        string secondsString = seconds.ToString("00");
+            string minutesString = minutes.ToString("00");
+            string secondsString = seconds.ToString("00");
 
-        timeText.text = minutesString + ":" + secondsString;
+            timeText.text = minutesString + ":" + secondsString;
+        }
+        else
+        {
+            timeText.text = "00:00";
+        }
     }
 }
