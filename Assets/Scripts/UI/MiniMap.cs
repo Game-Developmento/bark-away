acc965d3d19514e59ffee33444f3f4feec3f2403
oriHoward <ori.howard@gmail.com>
2023-06-10 20:44:47 +0300

    Added button to change minimap

--- Assets/Scripts/UI/MiniMap.cs
+++ Assets/Scripts/UI/MiniMap.cs
@@ -1,15 +1,40 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class MiniMap : MonoBehaviour
-// {
-//     public Transform player;
-//     private void LateUpdate()
-//     {
-//         Vector3 newPosition = player.position;
-//         newPosition.y = transform.position.y;
-//         transform.position = newPosition;
-//         // transform.rotation = Quaternion.Euler(90f, Camera.main.transform.eulerAngles.y, 0f);
-//     }
-// }
+public class MiniMap : MonoBehaviour
+{
+    public Transform player;
+    [SerializeField] private GameObject squareMiniMap;
+    [SerializeField] private GameObject circleMiniMap;
+    private Quaternion initialCameraRotation;
+    private Vector3 initialPosition;
+
+
+    private void Start()
+    {
+        initialCameraRotation = Quaternion.Euler(90f, Camera.main.transform.eulerAngles.y, 0f);
+        initialPosition = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        if (circleMiniMap.activeSelf)
+        {
+            Vector3 newPosition = player.position;
+            newPosition.y = transform.position.y;
+            transform.position = newPosition;
+            transform.rotation = Quaternion.Euler(90f, Camera.main.transform.eulerAngles.y, 0f);
+        }
+    }
+    public void ToggleDifferentMiniMap()
+    {
+        squareMiniMap.SetActive(!squareMiniMap.activeSelf);
+        circleMiniMap.SetActive(!circleMiniMap.activeSelf);
+        if (squareMiniMap.activeSelf)
+        {
+            transform.rotation = initialCameraRotation;
+            transform.position = initialPosition;
+        }
+    }
+}
