88fab188e79f05aaee6e7669a0ec3f7464b9cf84
adiy55 <adiyafe55@gmail.com>
2023-06-14 00:43:12 +0300

    Add pause button and menu

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseGame : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pauseMenu;
+    private bool isGameAlreadyPaused;
+    public void LoadMainMenuScene()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnGamePaused()
+    {
+        isGameAlreadyPaused = Time.timeScale == 0;
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void OnGameResumed()
+    {
+        if (!isGameAlreadyPaused)
+        {
+            Time.timeScale = 1;
+        }
+        pauseMenu.SetActive(false);
+    }
+}
