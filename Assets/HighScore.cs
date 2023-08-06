23596c0e8b2721f8a02e4b46921504dfbb1e948a
OriHoward <ori.howard@gmail.com>
2023-08-06 19:41:03 +0300

    Added highscore view for each level

---
+++ Assets/HighScore.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class HighScore : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro level1HighScoreText = null;
+    [SerializeField] private TextMeshPro level2HighScoreText = null;
+    [SerializeField] private TextMeshPro level3HighScoreText = null;
+    
+    private void Start() {
+        if (level1HighScoreText.text != null && PlayerPrefs.HasKey("Level 1_highScore")) {
+            int highScore = PlayerPrefs.GetInt("Level 1_highScore");
+            level1HighScoreText.text = "LEVEL 1     BEST: " + highScore;
+        }
+        if (level2HighScoreText.text != null && PlayerPrefs.HasKey("Level 2_highScore")) {
+            int highScore = PlayerPrefs.GetInt("Level 2_highScore");
+            level2HighScoreText.text = "LEVEL 2     BEST: " + highScore;
+        }
+        if (level3HighScoreText.text != null && PlayerPrefs.HasKey("Level 3_highScore")) {
+            int highScore = PlayerPrefs.GetInt("Level 3_highScore");
+            level3HighScoreText.text = "LEVEL 3     BEST: " + highScore;
+        }
+    }
+}
